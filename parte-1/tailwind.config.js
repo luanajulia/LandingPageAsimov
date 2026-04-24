28df88c9e3b2dd935c8d13e5d8d1dc082c99d549
Luana Julia <luana.ju.gomes@gmail.com>
2026-04-23 21:16:44 -0300

    Remove 'brand-light-bg' color from config
    Removed 'brand-light-bg' color from Tailwind config.

--- parte-1/tailwind.config.js
+++ parte-1/tailwind.config.js
@@ -10,7 +10,6 @@ export default {
       colors: {
         'brand-green': '#A1FD4B',
         'brand-dark': '#0F121C',
-        'brand-light-bg': '#F5F5F7',
       },
       fontFamily: {
         'sans': ['Space Grotesk', 'system-ui', 'Segoe UI', 'Roboto', 'sans-serif'],
